00469a58d5a87c917402d79b8f49849fd85d8886
amgc3 <adropofwate0@gmail.com>
2023-11-09 12:10:11 +0000

    add routing with compojure

--- src/firstclojureapp/core.clj
+++ src/firstclojureapp/core.clj
@@ -1,13 +1,20 @@
 (ns firstclojureapp.core
-  (:require [ring.adapter.jetty :refer [run-jetty]])
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [compojure.core :refer :all]
+            [compojure.route :as route])
   (:gen-class))
 
-(defn app-handler [request]
-  {:status 200
-    :headers {"Content-Type" "text/html"}
-    :body "<html><body><h1>Hello, there!</h1></body></html>"})
+(defroutes app
+           (GET "/" [] "<h1>Hello, World!</h1>")
+           (GET "/friends" [] "<h1>There are no friends yet </h1>"
+                ))
+
+;(defn app-handler [request]
+;  {:status 200
+;    :headers {"Content-Type" "text/html"}
+;    :body "<html><body><h1>Hello, there!</h1></body></html>"})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (run-jetty app-handler {:port 80}))
+  (run-jetty app  {:port 80}))
